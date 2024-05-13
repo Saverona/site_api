cf2011decbc169510f399f82a4e3b0155f84dc22
Saverona <saverona@bk.ru>
2024-05-13 20:57:41 +0300

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,35 +1,28 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig } from 'vitepress'
 
-// refer https://vitepress.dev/reference/site-config for details
+// https://vitepress.dev/reference/site-config
 export default defineConfig({
-  lang: 'en-US',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
-
+  title: "Vault API",
+  description: "You'll find comprehensive guides and documentation to help you start working with Vault as quickly as possible, as well as support if you get stuck",
   themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Example', link: '/example' },
-
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
-
-      // ...
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
-        // text: 'Guide',
+        text: 'Examples',
         items: [
-          { text: 'Example', link: '/example' },
-          // ...
-        ],
-      },
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
     ],
-  },
-});
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
--- docs/index.md
+++ docs/index.md
@@ -1,15 +1,25 @@
 ---
+# https://vitepress.dev/reference/default-theme-home-page
 layout: home
 
 hero:
-  name: VitePress
-  text: Vite & Vue powered static site generator.
-  tagline: Simple, powerful, and performant. Meet the modern SSG framework you've always wanted.
+  name: "Vault API"
+  text: "You'll find comprehensive guides and documentation to help you start working with Vault as quickly as possible, as well as support if you get stuck"
+  tagline: My great project tagline
   actions:
     - theme: brand
-      text: Get Started
-      link: /example
+      text: Markdown Examples
+      link: /markdown-examples
     - theme: alt
-      text: View on GitHub
-      link: https://github.com/vuejs/vitepress
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
+
